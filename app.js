7aee61fa2452dd902151594587984aa35a9d5a06
John Armbruster <jaboyak@gmail.com>
2016-11-04 12:00:01 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+var app = angular.module('myFirstNgApp', []);
 
+app.controller('myFirstController', function($scope, $http, myFirstFactory) {
+  $scope.makeAPIcall = function(character) {
+    myFirstFactory.getCharacter(character)
+     .then(function(api_response) {
+       console.log(api_response);
+       $scope.results = api_response.data.results;
+     });
+  }
+});
+
+app.service('myFirstService', function($http) {
+  this.getCharacter = function(character){
+    return $http.get('https://swapi.co/api/people/?search=' + character);
+  }
+});
+
+app.factory('myFirstFactory', function($http) {
+  return {
+    getCharacter: function(character){
+      return $http.get('https://swapi.co/api/people/?search=' + character);
+    }
+  }
+});
